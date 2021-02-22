52a5e871d2a3b9dca1ec15c09d00aa83b5fa41c4
Candilola <68983496+Candilola@users.noreply.github.com>
2021-02-22 17:30:24 +1030

    navigation links activated

--- JS/index.js
+++ JS/index.js
@@ -12,6 +12,34 @@
   }
 }
 
+$(document).ready(function(){
+    var btt = $(".back-to-top")
+
+    btt.on("click", function(e){
+        $("html, body").animate({
+            scrollTop: 0
+        }, 500);
+
+        e.preventDefault();
+    });
+
+    $(window).on("scroll", function(){
+        var self = $(this),
+        height = self.height(),
+        top = self.scrollTop();
+
+        if(top > height){
+            if(!btt.is(":visible")){
+                btt.show();
+            }
+        } else {
+            btt.hide();
+        }
+
+        console.log(height);
+    });
+});
+
 
 // function myFunction() {
 //     var x = document.getElementById("myLinks");
